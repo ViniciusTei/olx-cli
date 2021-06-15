e267d4c1b9db7a20591a31f33c6c35fb93f72014
Vincius <viniciustprates@gmail.com>
2021-06-15 10:15:14 -0300

    🚧 add command line interation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
+#!/usr/bin/env node
 import puppeteer from 'puppeteer'
+import { createInterface } from 'readline';
+
+const readLine = createInterface({
+    input: process.stdin,
+    output: process.stdout 
+})
+
+//Promisse to get user input, return the string entered
+const getUserInput = (pergunta: string) => new Promise<string>(resposta => readLine.question(pergunta, resposta));
 
 async function start() {
+    console.log("Bem vindo a OLX CLI")
+    console.log("Ente com um estado para comecar ou aperte enter: ")
+    //gets the prompt ready for the user input
+    readLine.prompt()
+    const estado = await getUserInput("🗽 Estado: ").then((res) => {
+        readLine.resume() //remember to always pause de prompt so the program can continue
+        return res
+    });
+
+    readLine.prompt()
+    const searchItem = await getUserInput("🗃️ O que voce esta buscando?: ").then((res) => {
+        readLine.resume() //remember to always pause de prompt so the program can continue
+        return res
+    });
+
+    const parameterItem = searchItem.replace(' ', '%20')
+
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto('https://example.com');
+    
+    await page.goto(`https://${estado}.olx.com.br/?q=${parameterItem}`);
     await page.screenshot({ path: 'example.png' });
 
     await browser.close();
